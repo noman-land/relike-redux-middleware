6215a27945d9818696696d0bc9aab1885617b4dc
Noman <noman@noman.land>
2017-08-03 18:49:48 -0400

    Add likeCount and myRating actions to export

--- js/redux/actions/ReLikeActions.js
+++ js/redux/actions/ReLikeActions.js
@@ -1,11 +1,15 @@
 import * as dislikeActions from './dislikeActions';
 import * as likeActions from './likeActions';
+import * as likeCountActions from './likeCountActions';
+import * as myRatingActions from './myRatingActions';
 import * as unDislikeActions from './unDislikeActions';
 import * as unLikeActions from './unLikeActions';
 
 export default {
   dislikeActions,
   likeActions,
+  likeCountActions,
+  myRatingActions,
   unDislikeActions,
   unLikeActions,
 };
