fa85e434cab685c2aa54721f4aef6ca69a5c4792
Noman <noman@noman.land>
2017-08-03 18:47:35 -0400

    Make and export ReLikeActions file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-import ReLikeMetaActions from './redux/actions/ReLikeMetaActions';
 import ReLikeActionTypes from './redux/actions/actionTypes';
+import ReLikeActions from './redux/actions/ReLikeActions';
+import ReLikeMetaActions from './redux/actions/ReLikeMetaActions';
 import ReLikeMiddleware from './redux/middleware/ReLikeMiddleware';
 
 export {
+  ReLikeActions,
   ReLikeActionTypes,
   ReLikeMetaActions,
 };
---
+++ js/redux/actions/ReLikeActions.js
@@ -0,0 +1,11 @@
+import * as dislikeActions from './dislikeActions';
+import * as likeActions from './likeActions';
+import * as unDislikeActions from './unDislikeActions';
+import * as unLikeActions from './unLikeActions';
+
+export default {
+  dislikeActions,
+  likeActions,
+  unDislikeActions,
+  unLikeActions,
+};
